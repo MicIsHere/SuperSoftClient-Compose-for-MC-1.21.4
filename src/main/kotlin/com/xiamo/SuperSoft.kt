807158e5d3345f3a6b206bfd6cf3d2655491afb1
XiaMo <2353163892@qq.com>
2025-11-28 09:45:01 +0800

    None

--- src/main/kotlin/com/xiamo/SuperSoft.kt
+++ src/main/kotlin/com/xiamo/SuperSoft.kt
@@ -7,7 +7,7 @@ import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
 object SuperSoft : ModInitializer {
-    private val logger = LoggerFactory.getLogger("supersoft")
+    val logger = LoggerFactory.getLogger("SuperSoft Client")
 
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
